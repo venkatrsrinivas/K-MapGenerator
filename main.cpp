7923aec33aaa645e9d8697730eed8990219897e7
Venkat Srinivas <venkat@gomotion.ai>
2020-01-18 14:31:25 -0500

    Added main.cpp For Initial Code-Planning.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	
+	if(argc < 2){
+		cerr << "Error in # Arguments" << endl;
+		return EXIT_FAILURE;
+	}
+	
+	return EXIT_SUCCESS;
+
+} 
