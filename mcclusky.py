360804a5395e7c9f687304360e1d951d2fd5010d
hsueha <alexandrahsueh@gmail.com>
2020-01-22 17:27:04 -0500

    outline of mccluskey and some code

--- mcclusky.py
+++ mcclusky.py
@@ -10,3 +10,24 @@
 # loop through list values and put into tables sorting by number of 1s
 # combine values in each adjacent group until nothing to combine left
 # spit out remaining indices and convert to binary to find minimized expression
+
+def checkTrue(input, index, variables):
+	# Takes in input and then for each variable inputs the value 
+	# we have been given in index form and returns truth value
+	val = indexTovals(index)
+	return false
+def indexTovals():
+	return None
+def countVars(input, variables):
+	return 0
+def McCluskey(input):
+	minterms = []
+	variables = []
+	varCount = countVars(input, variables)
+	num = pow(2, varCount) - 1
+	for i in range(num):
+		if checkTrue(input, i, variables):
+			minterms.append(i)
+	tables = [[] for i in range(varCount)]
+
+	# combining
