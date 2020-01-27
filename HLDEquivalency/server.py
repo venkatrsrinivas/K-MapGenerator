0b0f09f697acb7a72fc679c19291da8ee5df9332
Tobias <tobiasjpark73@gmail.com>
2020-01-27 09:34:44 -0500

    Adapted HLD-Equivalency to work without the HTML page

--- HLDEquivalency/server.py
+++ HLDEquivalency/server.py
@@ -1,26 +1,14 @@
 from __future__ import unicode_literals
-from flask import Flask, Markup, render_template, request
 import logical_equivalency
 import util
 
-FLASK_APP = Flask(__name__)
-
-
-@FLASK_APP.route("/")
-def index_page():
-    return render_template('index.html')
-
-
-@FLASK_APP.route("/submit", methods=['POST'])
-def generate_equivalency():
-    formula1 = request.form['formula1']
-    formula2 = request.form['formula2']
-    form = Markup(render_template('form.html', formula1=formula1, formula2=formula2))
+def generate_equivalency(formula1, formula2):
 
     try:
         equal, parse1, parse2 = logical_equivalency.runner(formula1, formula2)
     except (SyntaxError, TypeError) as exception:
-        return render_template('error.html', error=str(exception), form=form)
+        print(str(exception))
+        exit()
 
     final_steps1 = []
     for steps, step_type in parse1[1]:
@@ -30,10 +18,8 @@ def generate_equivalency():
     for steps, step_type in parse2[1]:
         final_steps2.append([util.pretty_print(steps[-1]), steps[:-1], util.StepTypes.get_message(step_type)])
 
-    return render_template('show.html', equal=equal, form=form, steps1=final_steps1, final_form1=parse1[0],
-                           steps2=final_steps2, final_form2=parse2[0])
+    print("Are the statements equivalent? " + str(equal))
+    print("Statement 1 CDNF: " + str(parse1[0]))
+    print("Statement 2 CDNF: " + str(parse2[0]))
 
-
-if __name__ == '__main__':
-    FLASK_APP.debug = True
-    FLASK_APP.run()
+generate_equivalency("A", "not(nots(A))") 
