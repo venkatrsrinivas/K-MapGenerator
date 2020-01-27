dad795517ca2e163124c32b70354de244e394b6d
hsueha <alexandrahsueh@gmail.com>
2020-01-27 15:00:41 -0500

    Just added a print around the equivCheck in main

--- Backend Code/main.py
+++ Backend Code/main.py
@@ -2,7 +2,7 @@ import sys;
 import equivCheck;
 
 def main(): 
-	equivCheck.generate_equivalency("A", "not(not(A))")
+	print(equivCheck.generate_equivalency("A", "not(not(A))"))
 
 if __name__ == '__main__':
 	main();
